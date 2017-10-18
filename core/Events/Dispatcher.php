6f278b906f501182761a2873057f628b439288c4
INANI EL HOUSSAIN <inanielhoussain@gmail.com>
2017-10-18 14:19:54 +0100

    Event Dispatcher

--- core/Events/Dispatcher.php
+++ core/Events/Dispatcher.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Events;
+namespace Chibi\Events;
 
 
 class Dispatcher {
--- core/Events/Listener.php
+++ core/Events/Listener.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Events;
+namespace Chibi\Events;
 
 
 abstract class Listener {
