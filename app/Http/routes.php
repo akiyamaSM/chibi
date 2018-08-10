f3d668cd45036aed1eea99d827d9057775ac4ca9
INANI EL HOUSSAIN <inanielhoussain@gmail.com>
2018-08-10 17:27:05 +0100

    Dumper

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,7 +5,10 @@ use Chibi\Request;
 $router->get('/user/{user}/{name}', 'App\Controllers\HomeController@views')->named('customers');
 $router->get('/customers', 'App\Controllers\HomeController@index');
 $router->get('/test', function() {
-    var_dump(getenv('NAME'));
+    $object = new StdClass();
+    $object->name = "Houssain";
+    dump(getenv('NAME'));
+    dump($object);
 });
 $router->get('/hola/{name}', function($name, $h) {
     echo route('customers', [
