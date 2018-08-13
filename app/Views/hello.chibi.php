3665f74ec2c72aadea4fe3f0bf3288ed38df07a7
El Houssain <inanielhoussain@gmail.com>
2018-08-14 00:10:05 +0100

    Can use internal variables too

--- app/Views/hello.chibi.php
+++ app/Views/hello.chibi.php
@@ -1,5 +1,7 @@
 {{ $name }} is {{ $age}}
 
+<?php $var = 30 ?> 
+
 @when( $age <= 10 )
 		You are a kid
 @or( $age > 10 )
@@ -10,3 +12,6 @@
 @foreach($array as $arr)
 		<h1>	{{ $arr }} </h1>
 @endforeach
+
+
+<h3>	{{ $var }}</h3>
