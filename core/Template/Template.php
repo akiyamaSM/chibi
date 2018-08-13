f45033f4c30c7c36e802bdd1a6452e3d2d4609a3
anaselalami <anaselalamikh@gmail.com>
2018-08-13 15:06:25 +0100

    check variable not exist in view template

--- core/Template/Template.php
+++ core/Template/Template.php
@@ -32,7 +32,9 @@ class Template {
      */
     public function compileEcho()
     {
-
+        if ($this->checkUnassingedVariables($value)) {
+            throw new \Exception("variable <b>$".$value."</b> is undefined");
+        }
         foreach($this->vars as $key => $value){
             $this->contents = preg_replace('/\{\{\s*\$'. $key .'\s*\}\}/', $value , $this->contents);
         }
@@ -53,5 +55,23 @@ class Template {
     public function render()
     {
         echo $this->contents;
+    }
+    /**
+     * [getTemplateVars description]
+     * @return [type] [description]
+     */
+    public function getTemplateVars() {
+        $matches = [];
+        preg_match_all('/{{\s*(\$(.*?))\s*}}/', $this->contents,$matches);
+        return isset($matches[2]) ? $matches[2] : [];
+    }
+
+    public function checkUnassingedVariables(&$value) {
+        $diff = array_diff($this->getTemplateVars(),array_keys($this->vars));
+        if (count($diff) > 0)  {
+
+            $value = array_values($diff)[0];
+        }
+        return count($diff) > 0;
     }
 }
