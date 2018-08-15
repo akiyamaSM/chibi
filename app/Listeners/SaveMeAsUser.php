12407872a608a7200676591c4e6af6700a8b5761
INANI EL HOUSSAIN <inanielhoussain@gmail.com>
2018-08-15 16:57:19 +0100

    Event Example

---
+++ app/Listeners/SaveMeAsUser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+use Chibi\Events\Event;
+use Chibi\Events\Listener;
+
+class SaveMeAsUser extends Listener {
+
+    /**
+     * @param Event $event
+     * @return mixed
+     */
+    public function handle(Event $event)
+    {
+        var_dump("You are Saved. Thanks!");
+        return true;
+    }
+}
---
+++ app/Listeners/SayHello.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+use Chibi\Events\Event;
+use Chibi\Events\Listener;
+
+class SayHello extends Listener {
+
+    /**
+     * @param Event $event
+     * @return mixed
+     */
+    public function handle(Event $event)
+    {
+        var_dump("Hello " . $event->name);
+        return true;
+    }
+}
