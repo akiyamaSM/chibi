5191b91e126773a87090ad09531c151fe1c4741b
Inani El Houssain <inanielhoussain@gmail.com>
2018-11-19 15:15:07 +0000

    Token generation & compiler

--- app/Views/token.Kolores.php
+++ app/Views/token.Kolores.php
@@ -1,6 +1,6 @@
 <form action=" <?php echo route('test_csrf') ?>" method="POST">
     <label for="name">Name</label>
     <input type="text" name="name" />
-    <input type="hidden" name="csrf_token" value="<?php echo \Kolores\Session\Session::put('csrf_token', '12345') ?>" />
+    @csrf_field
     <input type="submit">
 </form>
---
+++ core/Template/Compilers/CSRFCompiler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kolores\Template\Compilers;
+
+
+class CSRFCompiler implements Compilable {
+
+    /**
+     * Construct
+     *
+     * @param array $vars
+     */
+    public function __construct($vars = [])
+    {
+        $this->vars = $vars;
+    }
+
+    /**
+     * Compile if statement
+     *
+     * @param $content
+     * @return mixed
+     */
+    public function compile($content)
+    {
+        $token = \Kolores\Session\Session::put('csrf_token', substr(
+            base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 32)
+        );
+
+        $content = preg_replace('/@csrf_field/', "
+                <input type='hidden' name='csrf_token' value='$token' />
+            ", $content);
+        return $content;
+    }
+}
