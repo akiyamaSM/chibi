d400974cb29b6095a1b9eb53c6a614a1e20273bc
Inani El Houssain <inanielhoussain@gmail.com>
2018-11-19 10:20:35 +0000

    clean route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Post;
 use Kolores\App;
 
 $router->get('/user', 'App\Controllers\HomeController@views')->named('customers');
+
 $router->get('/configs', 'App\Controllers\HomeController@testConfig');
+
 $router->get('/test', function() {
     $om = App::getInstance()->getContainer()->om;
     /* @var $om Kolores\ObjectManager\ObjectManager */
@@ -14,53 +15,13 @@ $router->get('/test', function() {
     $dispatcher->addListeners("EntredLinkEvent", $om->resolve(\App\Listeners\SaveMeAsUser::class));
     $dispatcher->dispatch($om->create(\App\Events\EntredLinkEvent::class, [$name]));
 })->named('test');
+
 $router->get('/hola/{name}', function($name, $h) {
     echo route('customers', [
         $name, 'two'
     ]);
 })->named('Hola');
 
-$router->get('/testa', function() {
-    $om = App::getInstance()->getContainer()->om;
-    /* @var $om Kolores\ObjectManager\ObjectManager */
-    $testClass = $om->resolve(\App\Test\Test::class);
-})->named('testa');
-
-
 $router->get('/katana', function () {
     dump(\App\Friend::all());
-    die;
-    //$me = \App\Friend::find(2);
-    //$me->delete();
-    //die;
-    $new_Friend = new \App\Friend();
-    $new_Friend->blabla = "inanielhoussain@gmail.com";
-
-    var_dump($new_Friend->save());
-    die;
-    $friend = \App\Friend::findOrFail(10);
-    echo $friend->email;
-    die;
-    Post::destroy(6);
-
-    die;
-    $post = Post::find(1);
-    $post->name = "Bla Bla";
-    $post->save();
-
-    //var_dump(Post::destroy([4, 5]));
-    die();
-
-    $post_id = Post::find(1);
-    if(!is_null($post_id)){
-        echo $post_id->name;
-        die;
-    }
-
-    $post = new Post([
-        'last_name' => 'Inani'
-    ]);
-    $post->name = "Houssain";
-
-    echo $post->last_name;
 });
